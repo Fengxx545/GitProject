0a5a1c42cf141a1fbd04538a34e3c3cc23183b7c
Fengxx545 <569452415@qq.com>
2018-10-20 17:59:57 +0800

    weqe

--- app/src/main/java/com/ry/gitproject/activity/CoordinateTestActivity.java
+++ app/src/main/java/com/ry/gitproject/activity/CoordinateTestActivity.java
@@ -6,7 +6,11 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.ry.gitproject.R;
+import com.ry.gitproject.adapter.TestAdapter;
 import com.ry.gitproject.base.BaseActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -23,6 +27,9 @@ public class CoordinateTestActivity extends BaseActivity {
     @BindView(R.id.floatbt)
     FloatingActionButton mFloatbt;
 
+    private TestAdapter mTestAdapter;
+    private List<String> data = new ArrayList<>();
+
     @Override
     protected int getContentViewLayoutID() {
         return R.layout.activity_coordinate_test;
@@ -30,7 +37,11 @@ public class CoordinateTestActivity extends BaseActivity {
 
     @Override
     protected void initView(Bundle savedInstanceState) {
-        mRvToDoList.setAdapter();
+        for (int i = 0; i<= 100; i++){
+            data.add("我是第"+i+"个");
+        }
+        mTestAdapter = new TestAdapter(data);
+        mRvToDoList.setAdapter(mTestAdapter);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         mRvToDoList.setLayoutManager(layoutManager);
     }
